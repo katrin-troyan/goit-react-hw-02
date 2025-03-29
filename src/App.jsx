cf4dfd9dee7f7c0464cdaecdcf8ef99fb5313dad
Kateryna Troyan <ket.tro@gmail.com>
2025-03-29 12:01:12 +0200

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
 import './App.css';
-import userData from './userData.json';
-import Profile from './components/Profile/Profile';
-import friends from './friends.json';
-import FriendList from './components/FriendList/FriendList';
-import transactions from './transactions.json';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import { useState, useEffect } from 'react';
+import Description from './components/Description/Description';
+import Options from './components/Options/Options';
+import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification';
 
 export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    if (feedbackType === 'reset') {
+      setFeedback({ good: 0, neutral: 0, bad: 0 });
+    } else {
+      setFeedback(prevFeedback => ({
+        ...prevFeedback,
+        [feedbackType]: prevFeedback[feedbackType] + 1,
+      }));
+    }
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        avatar={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-
-      <TransactionHistory items={transactions} />
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
     </>
   );
 }
